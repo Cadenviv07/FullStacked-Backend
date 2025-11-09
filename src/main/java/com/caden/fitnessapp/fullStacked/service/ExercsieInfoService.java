43e51db86a4fc83645b95191ace9582f4b17555a
Cadenviv07 <cva701281@gmail.com>
2025-11-09 17:34:58 -0500

    Accidentally hardcoded api key

--- src/main/java/com/caden/fitnessapp/fullStacked/service/ExercsieInfoService.java
+++ src/main/java/com/caden/fitnessapp/fullStacked/service/ExercsieInfoService.java
@@ -1,17 +1,30 @@
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.stereotype.Service;
+import com.google.gson.Gson;
 
-@Servcice
+
+@Service
 public class ExercsieInfoService {
-    
-    public String getExerciseInfo (String exerciseName){
+
+    Gson gson = new Gson();
+
+    @Value("${exercisedb.api.key}")
+    private String apiKey;
+
+    @Value("${exercisedb.api.host}")
+    private String apiHost;
+
+    public Exercise getExerciseInfo (Exercise exercise){
+
         HttpRequest request = HttpRequest.newBuilder()
-		.uri(URI.create("https://exercisedb.p.rapidapi.com/exercises/targetList"))
-		.header("x-rapidapi-key", "0e9ed6b719msh6e45e95521e2dfcp18c26djsn0630a3e0a192")
-		.header("x-rapidapi-host", "exercisedb.p.rapidapi.com")
+		.uri(URI.create("https://exercisedb.p.rapidapi.com/exercises/name" + exercise.getName()))
+		.header("x-rapidapi-key", "apiKey")
+		.header("x-rapidapi-host", "apiHost")
 		.method("GET", HttpRequest.BodyPublishers.noBody())
 		.build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        exercise = gson.fromJson(response, exercise.class);
+        return exercise;
     }
 }
