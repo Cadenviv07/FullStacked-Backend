7bfe2b85f508379b92f8ada865aa8a4954d4ba37
Cadenviv07 <cva701281@gmail.com>
2025-11-13 14:15:13 -0500

    More bug fixes changing email verification to mongodb

--- src/main/java/com/caden/fitnessapp/fullStacked/model/VerificationCode.java
+++ src/main/java/com/caden/fitnessapp/fullStacked/model/VerificationCode.java
@@ -1,43 +1,32 @@
 package com.caden.fitnessapp.fullStacked.model;
 
 import java.time.LocalDateTime;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.Id;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-
-@Entity
-public class VerificationCode {
-    
+@Document(collection = "verification_codes")
+public class VerificationCode {   
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long Id;
+    private String id;
 
     private String code;
 
     private LocalDateTime expiryDate;
 
-    @OneToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    private String userId;
 
     public VerificationCode(){}
 
     public VerificationCode(String code, LocalDateTime expiryDate, User user){
         this.code = code;
         this.expiryDate = expiryDate;
-        this.user = user;
     }
 
-    public Long getId() { return Id; }
+    public String getId() { return id; }
     public String getCode() { return code; }
     public void setCode(String code) { this.code = code; }
     public LocalDateTime getExpiryDate() { return expiryDate; }
     public void setExpiryDate(LocalDateTime expiryDate) { this.expiryDate = expiryDate; }
-    public User getUser() { return user; }
-    public void setUser(User user) { this.user = user; }
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId;}
 }
