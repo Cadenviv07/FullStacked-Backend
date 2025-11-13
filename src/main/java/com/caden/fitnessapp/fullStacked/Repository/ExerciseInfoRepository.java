5ac5786cfc519c95d372c300f7512a6afce276ea
Cadenviv07 <cva701281@gmail.com>
2025-11-13 14:28:03 -0500

    test

--- src/main/java/com/caden/fitnessapp/fullStacked/Repository/ExerciseInfoRepository.java
+++ src/main/java/com/caden/fitnessapp/fullStacked/Repository/ExerciseInfoRepository.java
@@ -1,8 +1,7 @@
 package com.caden.fitnessapp.fullStacked.repository;
 
 import com.caden.fitnessapp.fullStacked.model.Exercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+;
 
 import java.util.Optional;
 
