bcd05758e94553386132990ba402d2706216435c
Cadenviv07 <cva701281@gmail.com>
2025-12-01 15:43:44 -0500

    local date

--- src/main/java/com/caden/fitnessapp/fullStacked/Repository/ExerciseLogRepository.java
+++ src/main/java/com/caden/fitnessapp/fullStacked/Repository/ExerciseLogRepository.java
@@ -1,8 +1,15 @@
+package com.caden.fitnessapp.fullStacked.repository;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.caden.fitnessapp.fullStacked.model.ExerciseLog;
+
 import java.time.LocalDate;
 import java.util.List;
 
-public interface ExerciseLogRepository extends MongoRepository<ExerciseLog, String> {
+public interface ExerciseLogRepository extends MongoRepository<ExerciseLog, LocalDate> {
     
     List<ExerciseLog> findByUserIdAndDate(String userId, LocalDate date);
+
+    List<ExerciseLog> findByUserIdandDateBetween(String userId, LocalDate startDate, LocalDate endDate);
 }
